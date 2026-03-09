4fc3369b8d874d8f51e5372085206759d25265b8
rom8069-oss <chris8069@gmail.com>
2026-03-09 11:20:26 -0500

    Refactor optimizer.js for improved readability

--- optimizer.js
+++ optimizer.js
@@ -1,54 +1,56 @@
-document.getElementById("optimizeBtn").onclick=function(){
+document.getElementById("optimizeBtn").onclick = function () {
 
 saveState()
 
-let stopW=Number(document.getElementById("stopWeight").value)
-let revW=Number(document.getElementById("revWeight").value)
-let distW=Number(document.getElementById("distWeight").value)
+let stopW = Number(document.getElementById("stopWeight").value)
+let revW = Number(document.getElementById("revWeight").value)
+let distW = Number(document.getElementById("distWeight").value)
 
-let reps=[...new Set(accounts.map(a=>a.newRep))]
+let reps = [...new Set(accounts.map(a => a.newRep))]
 
-let centers={}
+let centers = {}
 
-reps.forEach(r=>{
+reps.forEach(rep => {
 
-let pts=accounts.filter(a=>a.newRep===r)
+let pts = accounts.filter(a => a.newRep === rep)
 
-let lat=pts.reduce((s,a)=>s+a.lat,0)/pts.length
-let lng=pts.reduce((s,a)=>s+a.lng,0)/pts.length
+let lat = pts.reduce((s,a)=>s+a.lat,0) / pts.length
+let lng = pts.reduce((s,a)=>s+a.lng,0) / pts.length
 
-centers[r]={lat,lng}
+centers[rep] = {lat,lng}
 
 })
 
-accounts.forEach(a=>{
+accounts.forEach(a => {
 
-if(a.protected)return
+if (a.protected) return
 
-let best=a.newRep
-let bestScore=Infinity
+let bestRep = a.newRep
+let bestScore = Infinity
 
-reps.forEach(rep=>{
+reps.forEach(rep => {
 
-let c=centers[rep]
+let c = centers[rep]
 
-let dist=Math.hypot(a.lat-c.lat,a.lng-c.lng)
+let dist = Math.hypot(a.lat - c.lat, a.lng - c.lng)
 
-let score=
-dist*distW+
-rankWeight[a.rank]*stopW+
-a.sales*.0001*revW
+let score =
+dist * distW +
+rankWeight[a.rank] * stopW +
+a.sales * 0.0001 * revW
 
-if(score<bestScore){
-bestScore=score
-best=rep
+if (score < bestScore) {
+bestScore = score
+bestRep = rep
 }
 
 })
 
-a.newRep=best
+a.newRep = bestRep
 
-markers[a.id].setStyle({fillColor:getColor(best)})
+markers[a.id].setStyle({
+fillColor: getColor(bestRep)
+})
 
 })
 
